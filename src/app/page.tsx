aecea6f2adfb1c30c954d773948b111d996c289b
Mikoyzskie <rapidstrike13@gmail.com>
2024-04-13 20:03:16 +0800

    to be continued

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+
+
 import Image from "next/image"
 import Button from "@/components/Button"
 import Result from "@/components/Result";
@@ -19,8 +21,10 @@ export default async function Home() {
     return response.json()
   }
 
-  const test = await result("https://www.figma.com/file/VbwLtUFVGqRRiOcaBYcBZF/url-shortening-api-landing-page?type=design&node-id=0-107&mode=design&t=FrDFKnYjtEEpP5IV-0")
-  console.log(test);
+  function handleSumbit(e: any) {
+    console.log("test");
+
+  }
 
 
   return (
@@ -49,7 +53,7 @@ export default async function Home() {
       </main>
       <section className="relative bg-[#EFF1F7] px-6 pt-[160px] pb-[80px]">
         <div className="absolute -top-[77px] inset-x-0 mx-6 flex flex-col gap-6">
-          <div className="rounded-[10px] w-full bg-[#3A3054] relative p-6 overflow-hidden z-10 flex flex-col gap-[10px] px-6">
+          <div className="rounded-[10px] w-full bg-[#3A3054] relative p-6 overflow-hidden z-10  px-6">
             <Image
               src={"/bg-shorten-mobile.svg"}
               alt="bg pattern shorten"
@@ -57,18 +61,17 @@ export default async function Home() {
               height={128}
               className="absolute top-0 right-0 z-0"
             />
-            <input type="text" className="py-[6px] outline-[#F46363] border-[#F46363] border-[3px] px-[16px] leading-[36px] tracking-[#0.12px] relative z-10 w-full rounded-[5px]" placeholder="Shorten a link here..." />
-            <button className="text-lg text-white leading-[27px] font-bold rounded-[5px] relative z-10 w-full py-[10px] bg-[#2BD0D0]">Shorten It!</button>
+            <form action={() => { handleSumbit }} className="flex flex-col gap-4">
+              <input type="text" name="url" className="py-[6px] outline-[#F46363] border-[#F46363] border-[3px] px-[16px] leading-[36px] tracking-[#0.12px] relative z-10 w-full rounded-[5px]" placeholder="Shorten a link here..." />
+              <button type="submit" className="text-lg text-white leading-[27px] font-bold rounded-[5px] relative z-10 w-full py-[10px] bg-[#2BD0D0]">Shorten It!</button>
+            </form>
           </div>
-
-          <Result />
-          <Result />
-
         </div>
 
 
 
         <div className="flex flex-col gap-[92px]">
+          <Result />
           <div className="text-center flex flex-col gap-4">
             <h2 className="text-[28px] leading-[48px] font-bold -tracking-[0.7px] text-[#34313D]">Advanced Statistics</h2>
             <p className="text-[#9E9AA8] font-medium leading-[28px] tracking-[0.11px]">Track how your links are performing across the web with our advanced statistics dashboard.</p>
