d2f3b0c64ca988db01bb8a206642845319a18bac
Mikoyzskie <rapidstrike13@gmail.com>
2024-04-18 19:14:28 +0800

    copy api working

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -6,6 +6,8 @@ export default function Result({ item }: { item: string }) {
 
     const [copied, setCopied] = useState(false)
 
+    // setCopied(true)
+
     return (
         <div className='bg-white rounded-[5px] w-full'>
             <div className='py-[6px] px-4'>
@@ -14,7 +16,10 @@ export default function Result({ item }: { item: string }) {
             <div className='h-[1px] w-full bg-[#9E9AA8]'></div>
             <div className='p-6 pt-[6px] px-4'>
                 <h3 className='text-[#2BD0D0] mb-2 font-medium leading-9 tracking-[0.12px]'>{item}</h3>
-                <button onClick={() => setCopied(true)} className={clsx("text-lg text-white leading-6 font-bold rounded-[5px] relative z-10 w-full py-[10px]",
+                <button onClick={async () => {
+                    await navigator.clipboard.writeText(item);
+                    setCopied(true)
+                }} className={clsx("text-lg text-white leading-6 font-bold rounded-[5px] relative z-10 w-full py-[10px]",
                     copied ? "bg-[#3A3054]" : "bg-[#2BD0D0]"
                 )}>
                     {
